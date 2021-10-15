750172a2ac6f70b8e88fa567bf23e16b8f656990
Leonid Gadetsky <leonidgadetsky@yandex.ru>
2021-10-16 00:07:46 +0300

    add javadoc to javadoc branch

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"LinkedList"},{"p":"<Unnamed>","l":"LinkedListTest"},{"p":"<Unnamed>","l":"LinkedList.node"}];updateSearchResults();
